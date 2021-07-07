39fcb1f5a05aa35283d396317d4bf0c35a8ee436
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-07 13:06:56 +0800

    TipeDataNumberInt.php

---
+++ TipeDataNumberInt.php
@@ -0,0 +1,16 @@
+<?php
+echo "Decimal :";
+var_dump(1234);
+
+echo "Octal :";
+var_dump(01234);
+
+echo "Hexadecimal:";
+var_dump(0x1A);
+
+echo "Binary :";
+var_dump(0b11011);
+
+echo "Underscore in Number :";
+var_dump(200_000);
+?>
