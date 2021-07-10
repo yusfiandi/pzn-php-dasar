d1d3a46aecaa3c3383491f176b3ce653ed8d067b
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-10 08:38:40 +0800

    [TipeDataNumberInt.php]: Belajar tipe data float

--- TipeDataNumberInt.php
+++ TipeDataNumberInt.php
@@ -1,4 +1,5 @@
 <?php
+// Tipe Data - Integer
 echo "Decimal :";
 var_dump(1234);
 
@@ -13,4 +14,17 @@ var_dump(0b11011);
 
 echo "Underscore in Number :";
 var_dump(200_000);
+
+// Tipe Data - Float
+echo "Floating Point :";
+var_dump(1.234);
+
+echo "Floating Point dengan E notation : (1.7 * 1000) :";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E notation minus : (1.7 * 0.001) :";
+var_dump(1.7e-3);
+
+echo "Underscore Floating Point :";
+var_dump(1_234.123);
 ?>
