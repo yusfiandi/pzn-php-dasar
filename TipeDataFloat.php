16d4a4259564ec398ca45faaeace3bd8c2d4e3fb
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-10 14:13:45 +0800

    [TipeDataFloat.php] : Tipe data float

---
+++ TipeDataFloat.php
@@ -0,0 +1,14 @@
+<?php
+// Tipe Data Number - Float
+echo "Floating Point :";
+var_dump(1.234);
+
+echo "Floating Point dengan E notation : (1.7 * 1000) :";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E notation minus : (1.7 * 0.001) :";
+var_dump(1.7e-3);
+
+echo "Underscore Floating Point :";
+var_dump(1_234.123); 
+?>
