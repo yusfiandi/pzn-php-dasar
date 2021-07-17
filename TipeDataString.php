f01dc89df2be59958bcfaf2cf8b06874a03f2370
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-17 21:15:57 +0800

    TipeDataString : Membuat Tipe data string

---
+++ TipeDataString.php
@@ -0,0 +1,36 @@
+<?php 
+
+  // Tipe data String adalah tipe data representasi dari teks
+  // String bisa mengandung kosong atau banyak karakter
+
+  // Single Quote - Kutip 1 (')
+  // Membuat string dengan single quote / kutip 1
+
+  echo 'Name : ';
+  echo 'Yusfiandi';
+
+  // Double Quote - Kutip 2 (")
+  // Kelebihan dari double quote adalah bisa melakukan escape sequence
+  // Seperti contoh :
+  // \n untuk Enter
+  // \t untuk Tab
+  /* \" untuk double quote di dalam double quote */
+  //  Contoh : echo "<div class=\"container\">";
+  // kalo yg biasa echo "<div class='container'>";
+
+  echo "Nama : ";
+  echo "Yusfiandi";
+  // Contoh \n ENTER 
+  echo "\n";
+
+  echo "Bisa juga dipanggil ";
+  /* untuk double quote di dalam double quote \" \" */
+  echo "\"Yusfi\"";
+  echo "\n";
+
+  echo "Dan ini untuk tab ";
+  //  untuk \t TAB
+  echo "\t Halo \t Selamat \t Pagi";
+  echo "\n";
+ 
+?>
