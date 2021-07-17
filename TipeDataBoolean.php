24eb983254d094e8f5aed6f2d0fca51c7683addc
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-17 14:04:28 +0800

    [TipeDataBoolea.php : Tipe data Boolean.]

---
+++ TipeDataBoolean.php
@@ -0,0 +1,14 @@
+<?php
+
+  // Tipe Data Boolean tipe data paling sederhana di PHP
+  // Tipe Data Boolean adalah tipe data dengan nilai kebenaran (benar atau salah) atau (true or false)
+  // Nilai benar dipresentasikan dengan true (case insensitive)
+  // Nilai salah dipresentasikan dengan false (case insensitive)
+
+  echo "Benar : ";
+  var_dump(true);
+
+  echo "Salah : ";
+  var_dump(false);
+
+?>
