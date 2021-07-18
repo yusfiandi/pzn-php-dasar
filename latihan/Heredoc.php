289570c59f33feaa27b56f4b040fd70b2ffbabdb
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-18 15:01:21 +0800

    [LATIHAN]TipeDataString.php : Heredoc.php

---
+++ latihan/Heredoc.php
@@ -0,0 +1,18 @@
+<?php 
+  
+  // Multiline string
+  
+  // Fitur Heredoc
+  // Fitur untuk membuat string yang panjang. Sehingga kita tidak perlu manual.
+  // Melakukan enter, tab, dan yang lainnya secara manual.
+
+  // Heredoc mirip dengan double quote.
+
+  echo <<<FobiaSosial
+  Social anxiety disoreder atau fobia sosial
+
+  Adalah gangguan kesehatan mental yang ditandai dengan rasa takut akan diawasi, dihakimi, atau dipermalukan oleh orang lain.
+  Fobia sosial juga memiliki nama lain, yaitu: Gangguan kecemasan sosial.
+  FobiaSosial;
+
+?>
