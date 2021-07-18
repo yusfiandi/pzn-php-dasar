61a11454820999e2f358fb0acf73eb30785d9f2e
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-18 14:16:18 +0800

    [LATIHAN]TipeDataString.php : StringDoubleQuote.php

---
+++ latihan/StringDoubleQuote.php
@@ -0,0 +1,12 @@
+<?php 
+
+  // Tipe Data String
+  
+  // Double Quote - Kutip 2 "
+  
+  echo "Insecure ";
+  echo "adalah perasaan cemas, tidak mampu, kurang percaya diri ";
+  echo "yang membuat seseorang merasa tidak aman.";
+  
+
+?>
