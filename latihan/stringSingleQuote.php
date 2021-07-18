590a43ede79fde53893ee01aaa6e107cb23c827e
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-18 14:07:35 +0800

    [LATIHAN]TipeDataString.php : StringSingleQuote.php

---
+++ latihan/stringSingleQuote.php
@@ -0,0 +1,11 @@
+<?php
+
+  // Tipe Data String
+
+  // Single Quote - Kutip 1 '
+
+  echo 'Depresi. ';
+  echo 'adalah gangguan suasana hati (mood) yang di tandai dengan ';
+  echo 'perasaan sedih dan rasa tidak peduli.';
+
+?>
