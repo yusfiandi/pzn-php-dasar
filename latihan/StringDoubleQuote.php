b3eaa618bbe77326baa0157ad8d98eb2db86b691
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-18 14:49:08 +0800

    [LATIHAN]TipeDataStirng.php : Menambahkan Escape Sequence didalam Double Quote

--- latihan/StringDoubleQuote.php
+++ latihan/StringDoubleQuote.php
@@ -7,6 +7,33 @@
   echo "Insecure ";
   echo "adalah perasaan cemas, tidak mampu, kurang percaya diri ";
   echo "yang membuat seseorang merasa tidak aman.";
-  
+
+  // Double Quote - Kutip 2 "
+  // Dengan Escape Sequence
+
+  // \n untuk ENTER
+  echo "\n\n";
+  echo "Psikologi";
+  echo "\n";
+  echo "Psikologi mencakup empat bidang : ";
+  echo "\n";
+  echo "- Kesehatan Mental";
+  echo "\n";
+  echo "- Kondisi Emosional";
+  echo "\n";
+  echo "Cara pikiran mengelola informasi";
+  echo "\n";
+  echo "Perilaku sosial Manusia";
+  echo "\n\n";
+
+  // \t untuk TAB
+  echo "INSOMNIA";
+  echo "\n";
+  echo "Adalah \t Gangguan \t tidur";
+  echo "\n"; 
+  echo "Yang \t menyebabkan \t penderitanya \t sulit \t tidur";
+  echo "\n";
+  echo "Atau \t tidak \t cukup \t tidur \t waktu \t untuk \t melakukannya.";
+  echo "\n";  
 
 ?>
