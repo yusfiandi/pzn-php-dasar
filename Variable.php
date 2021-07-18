e79a655bb4b4439f9b254b20119096cc27d52583
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-18 16:06:57 +0800

    Variable.php : Variable.php

---
+++ Variable.php
@@ -0,0 +1,33 @@
+<?php
+
+  // Variable
+  // Variable adalah tempat menyimpan data.
+  // sehingga bisa kita gunakan lagi di kode program SELANJUTNYA
+
+  // PHP variable / Variable di PHP
+  // Bisa menampung berbagai jenis tipe data dan bisa berubah-ubah tipe data
+
+  // Variable PHP : $
+
+  // Variable php tidak boleh mengandung SPASI / SPACE ???
+
+  $nama = "Yusfiandi";
+  $umur = 19;
+
+  echo "Halo, Nama ku $nama";
+  echo "\n";
+  echo "Umur saya $umur";
+  echo "\n";
+
+  // Atau bisa juga
+  
+  echo "Halo, Nama ku ";
+  echo $nama;
+  echo "\n";
+  echo "Umur saya ";
+  echo $umur;
+  echo "\n";
+
+  // Variable variables
+
+?>
