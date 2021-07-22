c78b254a933d7d676341b150a7e088f27525f00b
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-22 16:05:56 +0800

    HapusVariable.php : Masih tentang Tipe data Null

---
+++ HapusVariable.php
@@ -0,0 +1,20 @@
+<?php 
+
+  // Menghapus Variable
+  
+  // Selain mengubah menjadi null, di PHP juga kita bisa menghapus sebuah variable
+  // Caranya dengan menggunakan function unset($variable)
+  // function unset($variable)
+
+  // Namun hati-hati, ketika kita hapus variable, kita tidak bisa lagi mengakses variable tersebut
+  // bahkan function is_null($variable) pun akan menjadi error jika mengakses variable tersebut
+
+  // Agar lebih aman, kita bisa menggunakkan function isset($variable)
+  // function isset($variable)
+
+  // untuk mengecek apakah sebuah variable ada dan nilainya tidak null
+
+  
+
+
+?>
