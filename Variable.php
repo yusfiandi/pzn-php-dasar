3e56baf8206feb86b8e04cd5d05858f284b1d416
Yusfiandi <yusfiandi.work@gmail.com>
2021-07-22 10:30:12 +0800

    Variable.php : Belajar Membuat Variable dari isi variable

--- Variable.php
+++ Variable.php
@@ -17,7 +17,7 @@
   echo "Halo, Nama ku $nama";
   echo "\n";
   echo "Umur saya $umur";
-  echo "\n";
+  echo "\n\n";
 
   // Atau bisa juga
   
@@ -26,8 +26,31 @@
   echo "\n";
   echo "Umur saya ";
   echo $umur;
-  echo "\n";
+  echo "\n\n";
 
   // Variable variables
+  
+  // PHP memiliki variable variables, yaitu variable dari string value variable
+  
+  // Walaupun fitur ini ada, tapi fitur ini sangat membingungkan jika digunakan secara luas
+  // Jadi disarankan untuk tidak menggunakan fitur ini kecuali memang diperlukan
+  
+  // Untuk membuat variable dari value variable kita bisa menggunakan $$ di ikuti dengan nama variablenya
 
-?>
+  $namaKu = "Yusfi";
+  $$namaKu = "Pede";
+
+  // ATAU
+  // $Yusfi = "Pede";
+
+  echo "\$namaKu : ";
+  echo $namaKu;
+  echo "\n";
+  echo "Yusfi : ";
+  echo $Yusfi;
+  echo "\n";
+  echo "Yusfi juga : ";
+  echo $Yusfi;
+  echo "\n\n";
+
+?>  
